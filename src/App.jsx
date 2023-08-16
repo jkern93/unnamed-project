7c85811cebf953d80e48690252aa4ccdfa47cc19
Jonathan Kern <jkern93@gmail.com>
2023-08-16 14:14:25 -0400

    Add dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,35 @@
 import { useState } from "react";
 import "./App.css";
 
-function App() {
+export default function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
       <h1>Unnamed Project</h1>
       <p className="read-the-docs">Here's the project</p>
+
+      <Dashboard></Dashboard>
     </>
   );
 }
 
-export default App;
+function Dashboard() {
+  return (
+    <section className="dashboard">
+      <Card></Card>
+      <Card></Card>
+      <Card></Card>
+      <Card></Card>
+    </section>
+  );
+}
+
+function Card() {
+  return (
+    <div className="card">
+      <h3 className="title">This is the card title</h3>
+      <p className="body">This is the card body</p>
+    </div>
+  );
+}
